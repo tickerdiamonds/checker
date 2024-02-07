157186153282ce515be2cbfdbd5c6b3ef1392d2c
tickerdiamonds <159243362+tickerdiamonds@users.noreply.github.com>
2024-02-07 12:47:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function checkWallet() {
         var walletList = data.wallets;
         var resultDiv = document.getElementById("result");
         if (walletList.includes(walletAddress)) {
-            resultDiv.innerHTML = "You are eligible for 100 000 $DIAMONDS";
+            resultDiv.innerHTML = "You are eligible for 100 000 $DIAMONDS 🎉";
         } else {
-            resultDiv.innerHTML = "You are not eligible :(";
+            resultDiv.innerHTML = "You are not eligible 😭";
         }
     })
     .catch(error => console.error('Error fetching wallets.json:', error));
